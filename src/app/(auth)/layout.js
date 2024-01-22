b22a0eaf7d4ddcce9e38f384721acd7e700707be
saurabhsathe <sathesaurabh30@gmail.com>
2024-01-21 21:08:58 -0800

    created route group layout by added the layout.js file in the auth group folder

---
+++ src/app/(auth)/layout.js
@@ -0,0 +1,10 @@
+export default function ProductLayouts({ children }) {
+    return (
+  
+        
+        <body > auth system layout header  {children}     auth system layout footer</body>
+  
+  
+    );
+  }
+  
