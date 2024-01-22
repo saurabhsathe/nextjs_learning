813b9388e7932002b260aeccd5a865a237b8d8a7
saurabhsathe <sathesaurabh30@gmail.com>
2024-01-21 20:11:42 -0800

    layout was added

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+
+      
+      <body className={inter.className}><h1>Header 1 </h1>{children}      <h1>Footer </h1>
+</body>
+
     </html>
   );
 }
